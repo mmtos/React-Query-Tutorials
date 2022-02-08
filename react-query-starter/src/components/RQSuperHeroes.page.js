6a21fb329974ba7a69e0d7c52bcb94b1ce45039d
mmtos <js223kr92@naver.com>
2022-02-08 17:16:13 +0900

    useQueryTutorial-1

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -1,3 +1,22 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const fetchSuperHeros = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
 export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>
-}
+  const queryKey = "super-heroes";
+  const { data, isLoading } = useQuery(queryKey, fetchSuperHeros);
+
+  if (isLoading) {
+    return <h2>Loading..</h2>;
+  }
+  return (
+    <>
+      <h2>React Query Super Heroes Page</h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
+    </>
+  );
+};
