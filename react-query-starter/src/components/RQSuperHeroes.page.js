129c40a909c4044302ba233677c4e8acb42e5828
mmtos <js223kr92@naver.com>
2022-02-08 17:23:53 +0900

    error handling

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -2,15 +2,24 @@ import axios from "axios";
 import { useQuery } from "react-query";
 
 const fetchSuperHeros = () => {
-  return axios.get("http://localhost:4000/superheroes");
+  return axios.get("http://localhost:4000/superheroes1");
 };
 export const RQSuperHeroesPage = () => {
   const queryKey = "super-heroes";
-  const { data, isLoading } = useQuery(queryKey, fetchSuperHeros);
+  const { data, isLoading, isError, error } = useQuery(
+    queryKey,
+    fetchSuperHeros
+  );
 
   if (isLoading) {
     return <h2>Loading..</h2>;
   }
+
+  if (isError) {
+    //retry가 기본적으로 설정되어 있어서 좀더 늦게 에러메시지가 뜸.
+    return <h2>{error.message}</h2>;
+  }
+
   return (
     <>
       <h2>React Query Super Heroes Page</h2>
