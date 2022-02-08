38f8d02940617ef0fa7240cd89bb124dae428667
mmtos <js223kr92@naver.com>
2022-02-08 20:11:03 +0900

    perform side effect after query

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -1,28 +1,45 @@
 import axios from "axios";
+import { useState } from "react";
 import { useQuery } from "react-query";
 
 const fetchSuperHeros = () => {
-  return axios.get("http://localhost:4000/superheroes");
+  return axios.get("http://localhost:4000/superheroes1");
 };
 const queryOptions = {
   cacheTime: 1000 * 60 * 5,
   staleTime: 1,
   refetchOnWindowFocus: true,
   refetchOnMount: true,
-  //polling
-  refetchInterval: 2000,
-  //polling when user are not focus the window
-  refetchIntervalInBackground: true,
+
+  // //polling
+  // refetchInterval: 2000,
+  // //polling when user are not focus the window
+  // refetchIntervalInBackground: true,
 };
 const autoQueryDisableOptions = { enabled: false };
 
 export const RQSuperHeroesPage = () => {
   const queryKey = "super-heroes";
+  const [_refetchInterval, set_refetchInterval] = useState(3000);
+  const onSuccess = (data) => {
+    console.log("perform side effect after query", data);
+    if (data.data.length === 4) {
+      set_refetchInterval(false);
+    }
+  };
+  const onError = (error) => {
+    console.log("perform side effect after encountering error", error);
+    set_refetchInterval(false);
+  };
+
   const { data, isLoading, isError, error, isFetching, refetch } = useQuery(
     queryKey,
     fetchSuperHeros,
-    //queryOptions
-    autoQueryDisableOptions
+    Object.assign(queryOptions, {
+      onSuccess,
+      onError,
+      refetchInterval: _refetchInterval,
+    })
   );
 
   console.log({ isLoading, isFetching });
