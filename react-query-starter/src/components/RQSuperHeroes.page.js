fb29dc9e883daba319a67d796b11f59df94096b6
mmtos <js223kr92@naver.com>
2022-02-12 18:49:31 +0900

    mutation & query invalidation

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { useSuperHeroesData } from "../hooks/useSuperHeroesData";
+import {
+  useSuperHeroesData,
+  useAddSuperHeroData,
+} from "../hooks/useSuperHeroesData";
 
 export const RQSuperHeroesPage = () => {
+  const [name, setName] = useState("");
+  const [alterEgo, setAlterEgo] = useState("");
   const [_refetchInterval, set_refetchInterval] = useState(3000);
+
   const onSuccess = (data) => {
     console.log("perform side effect after query", data);
     if (data.data.length === 4) {
@@ -18,7 +24,15 @@ export const RQSuperHeroesPage = () => {
   const { data, isLoading, isError, error, isFetching, refetch } =
     useSuperHeroesData(onSuccess, onError, _refetchInterval);
 
+  const { mutate } = useAddSuperHeroData();
+
   console.log({ isLoading, isFetching });
+
+  const handleAddHeroClick = () => {
+    console.log(name, alterEgo);
+    mutate({ name, alterEgo });
+  };
+
   if (isLoading || isFetching) {
     // default : 5분동안 cache
     return <h2>Loading..</h2>;
@@ -32,6 +46,21 @@ export const RQSuperHeroesPage = () => {
   return (
     <>
       <h2>React Query Super Heroes Page</h2>
+
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      ></input>
+      <input
+        type="text"
+        value={alterEgo}
+        onChange={(e) => setAlterEgo(e.target.value)}
+      ></input>
+      <button type="button" onClick={handleAddHeroClick}>
+        Add Hero
+      </button>
+
       <button onClick={refetch}>fetch heroes</button>
       {data.data?.map((hero) => {
         return (
--- react-query-starter/src/hooks/useSuperHeroesData.js
+++ react-query-starter/src/hooks/useSuperHeroesData.js
@@ -1,8 +1,13 @@
 import axios from "axios";
-import { useQuery } from "react-query";
+import { useQuery, useMutation, useQueryClient } from "react-query";
 
 const fetchSuperHeros = () => {
   return axios.get("http://localhost:4000/superheroes");
+};
+
+const addSuperHero = (hero) => {
+  //json-server에서 post 방식 제공함
+  return axios.post("http://localhost:4000/superheroes", hero);
 };
 
 const queryOptions = {
@@ -30,4 +35,16 @@ export const useSuperHeroesData = (onSuccess, onError, refetchInterval) => {
       _refetchInterval,
     })
   );
+};
+
+export const useAddSuperHeroData = () => {
+  // key가 필요 없음. 첫번째 인자는 mutation Function
+  const queryClient = useQueryClient();
+  return useMutation(addSuperHero, {
+    // what is onSettle
+    onSuccess: () => {
+      // invalidate -> refetch
+      queryClient.invalidateQueries("super-heroes");
+    },
+  });
 };
